5a27336d12560f2ef78fe7cd87b01125ec14eb38
Canop <cano.petrole@gmail.com>
2013-10-17 16:10:43 +0200

    first addition of the relevant files

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 tzdetect.js
 ===========
 
-A JavaScript library to detect the timezone using Moment.js
+
+Warning
+-------
+
+This is experimental. This seems to work, as for now, but it's mostly the basis for the elaboration of something stronger, maybe a proper extension or plugin for Moment.js. Changes brought with ES6 are in the radar.
+
+Goals
+-----
+
+* list all IANA time zone ids that are compatible with the javascript engine
+* limit data duplication by using the Moment.js version of the Olson tables
+
+Demo
+----
+
+See http://dystroy.org/stackoverflow/timezonedetect.html
+
+Usage
+-----
+
+	tzid = tzdetect.matches()[0]; // gives the IANA name of a time zone compatible with the browser's one
+
+Related
+-------
+
+	http://stackoverflow.com/questions/19420582/detect-the-id-of-the-current-user-timezone-using-moment-js
---
+++ tzdetect.js
@@ -0,0 +1,27 @@
+// a small library using moment.js, detecting the user's timezone
+// Repository : https://github.com/Canop/tzdetect.js
+// It must be included between the moment-timezone.js script and the moment-timezone-data.js one.
+// Usage :
+//   tzdetect.names : an array of all available timezone id
+//   tzdetect.matches() : returns an array of all timezones matching the user's one
+var tzdetect = {
+	names:[],
+	matches: function(){
+		var results = [], now = Date.now(), makekey = function(id){
+			return [0, 4, 8, -5*12, 4-5*12, 8-5*12, 4-2*12, 8-2*12].map(function(months){
+				var m = moment(now + months*30*24*60*60*1000);
+				if (id) m.tz(id);
+				return m.format("DDHH")
+			}).join(' ');
+		}, lockey = makekey();
+		tzdetect.names.forEach(function(id){
+			if (makekey(id)===lockey) results.push(id);
+		});
+		return results;
+	}
+};
+tzdetect.originalTzAdd = moment.tz.add;
+moment.tz.add = function(data){
+	if (data.zones) tzdetect.names = tzdetect.names.concat(Object.keys(data.zones));
+	tzdetect.originalTzAdd.call(moment.tz, data);
+}
